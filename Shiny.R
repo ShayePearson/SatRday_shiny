fe68707a446db86b6ae433f4ce8611de85364aac
ShayePearson <shaye_pearson@hotmail.com>
2017-02-17 10:49:12 +0200

    first app

---
+++ Shiny.R
@@ -0,0 +1,9 @@
+install.packages(c("shiny",
+                   "flexdashboard",
+                   "dplyr",
+                   "ggplot2",
+                   "leaflet",
+                   "DT",
+                   "devtools"))
+
+devtools::install_github("juliasilge/southafricastats")
